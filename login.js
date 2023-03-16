aee5af835f70bfda68a2e647085a74e5d788b4d4
Andres <cesarandrestorresbernal604@gmail.com>
2023-03-16 07:54:29 -0500

    proyecto mejorado

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 const titulo = document.querySelector(".titulo");
 function cambiarTitulo() {
     titulo.style.color = "red";
-    titulo.style.marginLeft = "30%";
     titulo.innerHTML = "Se ha guardado la infomación con éxito"
 }
