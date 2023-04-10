eecd025435eea91c038e90bf32cedf8241168aee
Andres <cesarandrestorresbernal604@gmail.com>
2023-04-10 09:55:17 -0500

    arreglo material desing

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const titulo = document.querySelector(".titulo");
+const titulo = document.querySelector(".h1");
 function cambiarTitulo() {
     titulo.style.color = "red";
     titulo.innerHTML = "Se ha guardado la infomación con éxito"
